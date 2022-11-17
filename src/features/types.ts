6dcf7bcaec30d491513b2616cae35b7d53161e68
Serey Roth <sereyratanakroth@gmail.com>
2022-11-16 22:35:40 -0800

    rewrote game types based on returned json

--- src/features/types.ts
+++ src/features/types.ts
@@ -1,17 +1,18 @@
 export interface Game {
-    id: 'number';
-    slug: 'string';
-    name: 'string';
-    description: 'string';
-    released: 'string';
-    background_image: 'string';
-    background_image_additional: 'string';
-    rating: 'number';
+    id: string | number;
+    slug: string | number;
+    name: string | number;
+    released: string | number;
+    background_image: string | number;
+    background_image_additional: string | number;
+    rating: string | number;
+    genres: string | number;
+    dominant_color: string | number;
 }
 
 export interface GamesList {
-    count: 'number';
-    next?: 'string';
-    previous?: 'string';
-    results: Game[] | [];
+    count: number;
+    next?: string;
+    previous?: string;
+    results: [];
 }
