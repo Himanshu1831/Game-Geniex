b4fc14b6a829160110a7138a4f4e6cf80ec627e1
Serey Roth <sereyratanakroth@gmail.com>
2022-11-17 21:41:06 -0800

    added transformResponse for type safety

--- src/redux/api/gameAPI.ts
+++ src/redux/api/gameAPI.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { GamesList } from '../../features/types'
+import { GamesListType, GameType } from '../../features/types';
 
 const API_KEY = '216afc1f9f044e1daafe76a78da14b71'
 
@@ -13,8 +13,11 @@ export const gameApi = createApi({
     reducerPath: 'gameApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.rawg.io/api/'}),
     endpoints:(builder) => ({
-        getGamesList: builder.query<GamesList, args>({ 
+        getGamesList: builder.query<ReturnType<typeof GamesListType>, args>({ 
             query: ({page, pageCount}) => `games?page=${page}&page_size=${pageCount}&key=${API_KEY}`,
+            transformResponse: (rawResult: unknown) => {
+                return GamesListType(rawResult)
+            }
         })
     })
 })
