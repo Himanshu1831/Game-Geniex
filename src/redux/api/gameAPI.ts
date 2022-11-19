155e5653dc12a2b24a7eae023a1bad4e2dad7fbe
Serey Roth <sereyratanakroth@gmail.com>
2022-11-18 21:28:58 -0800

    changed endpoint for no query param

--- src/redux/api/gameAPI.ts
+++ src/redux/api/gameAPI.ts
@@ -1,20 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { GamesListType, GameType } from '../../features/types';
+import { GamesListType } from '../../features/typeGuards'
 
 const API_KEY = '216afc1f9f044e1daafe76a78da14b71'
 
-type args = {
+type Query = {
     page: number;
     pageCount: number;
+}
+
+type Args = Query | {}
+
+const isQuery = (args: Args): args is Query => {
+    return (args as Query).page !== undefined && 
+    (args as Query).pageCount !== undefined
 }
 
 export const gameApi = createApi({
     reducerPath: 'gameApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.rawg.io/api/'}),
     endpoints:(builder) => ({
-        getGamesList: builder.query<ReturnType<typeof GamesListType>, args>({ 
-            query: ({page, pageCount}) => `games?page=${page}&page_size=${pageCount}&key=${API_KEY}`,
+        getGamesList: builder.query<ReturnType<typeof GamesListType>, Args>({ 
+            query: (args) => {
+                if (isQuery(args)) {
+                    return `games?page=${args.page}&page_size=${args.pageCount}&key=${API_KEY}`
+                }
+                return `games?&key=${API_KEY}`
+            },
             transformResponse: (rawResult: unknown) => {
                 return GamesListType(rawResult)
             }
