0b661b18f44bc1e47520b0b9842efd430490cc6f
Serey Roth <sereyratanakroth@gmail.com>
2022-11-18 22:28:36 -0800

    removed intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
-import { useRef, useLayoutEffect, useState, useCallback } from 'react'
 import { Container } from '@mui/material'
-
-import { useGetGamesListQuery } from './redux/api/gameAPI'
-
 import GameCards from './components/cards/GameCards'
 import GameTable from './components/table/GameTable'
 
 function App() {
-    const [pageInfo, setPageInfo] = useState({
-        page: 0,
-        pageCount: 20,
-    })
-    const lastElement = useRef<HTMLDivElement>(null)
-	const { data, isFetching } = useGetGamesListQuery({page: pageInfo.page + 1, pageCount: pageInfo.pageCount})
-
-    const handleIntersection = useCallback((entries) => {
-        if (entries[0].isIntersecting) {
-            setPageInfo((prev) => {
-                return { 
-                    ...prev,
-                    page: prev.page + 1,
-                }
-            });
-        }
-    }, []);
-
-    useLayoutEffect(() => {
-        const options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0,
-        }
-        const observer = new IntersectionObserver(handleIntersection, options)
-        if (lastElement.current) {
-            observer.observe(lastElement.current)
-        }
-    }, [])
-
     return (
         <Container sx={{
             width: '100vw',
@@ -46,8 +12,7 @@ function App() {
             justifyContent: 'center',
             position: 'relative',
         }} maxWidth={false}>
-            <GameCards data={data} isFetching={isFetching} pageInfo={pageInfo}/>
-            <div ref={lastElement}></div>
+            <GameTable />
         </Container>
     )
 }
