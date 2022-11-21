ebc7b1a761e63938b68f3ffd7866c99956d0f66a
Serey Roth <sereyratanakroth@gmail.com>
2022-11-20 18:06:50 -0800

    created Image Component with loading status

---
+++ src/components/LoadedImage.tsx
@@ -0,0 +1,54 @@
+import React, { HTMLAttributes, useState, useEffect } from 'react'
+
+import CardMedia from '@mui/material/CardMedia'
+import { Skeleton } from '@mui/material';
+
+interface Props extends HTMLAttributes<HTMLImageElement> {
+    height?: number | string;
+    src: string;
+    name: string;
+}
+
+enum Status {
+    Pending,
+    Success,
+    Failure
+} 
+
+const LoadedImage = (props: Props) => {
+    const { height, src, name, ...htmlAttributes } = props;
+    const [status, setStatus] = useState<Status>(Status.Pending);
+    const [image, setImage] = useState<HTMLImageElement>(null);
+    
+    useEffect(() => {
+        const image = new Image();
+        image.onload = () => setStatus(Status.Success)
+        image.onerror = () => setStatus(Status.Failure)
+        image.src = src;
+        setImage(image)
+    }, [])
+    
+
+    return (
+        <>
+            {status === Status.Success ? (
+                <CardMedia 
+                component='img'
+                image={image.src}
+                alt={name} 
+                {...htmlAttributes}
+                sx={{
+                    width: '100%', 
+                    height: height,
+                    objectPosition: 'top',
+                    objectFit: 'cover'
+                }} />
+            ) : (
+                <Skeleton variant='rectangular' 
+                width='100%' height={height} animation='wave' />
+            )}
+        </>
+    )
+}
+
+export default LoadedImage
