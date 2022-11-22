2274a5e9807b2cd96cea710442af7d6d41bf0829
Serey Roth <sereyratanakroth@gmail.com>
2022-11-21 20:58:04 -0800

    fix page navigation issues

--- src/components/pagination/PaginationArrows.tsx
+++ src/components/pagination/PaginationArrows.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useCallback, startTransition } from 'react'
 
+import IconButton from '@mui/material/IconButton'
+
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
 import { PaginationProps } from '.'
@@ -14,25 +16,34 @@ const PaginationArrows = ({ page, setPage, rowsPerPage, totalGameCount }: Props)
     const handlePrevious = useCallback(() => {
         startTransition(() => {
             if (page === 0) return;
-            setPage(page - 1);
+            setPage(prev => prev - 1);
         })
-    }, [])
+    }, [page])
 
     const handleNext = useCallback(() => {
         startTransition(() => {
             if (!totalGameCount) return;
             if (page * rowsPerPage >= totalGameCount) return;
-            setPage(page + 1);
+            setPage(prev => prev + 1);
         })
-    }, [])
+    }, [page, rowsPerPage, totalGameCount])
 
     return (
         <>
-            <BsArrowLeftCircleFill className={`pagination-arrow ${page === 0 ? 'inactive' : ''}`}
-            onClick={handlePrevious}/>
-            <BsArrowRightCircleFill className={`pagination-arrow 
-            ${typeof totalGameCount === 'number' && (page * rowsPerPage >= totalGameCount) ? 'inactive' : ''}`}
-            onClick={handleNext}/>
+            <IconButton 
+            onClick={handlePrevious} 
+            sx={{ position: 'fixed', top: '50%'}} 
+            disableRipple>
+                <BsArrowLeftCircleFill className={`pagination-arrow ${page === 0 ? 'inactive' : ''}`} />
+            </IconButton>
+            <IconButton 
+            onClick={handleNext} 
+            sx={{ position: 'fixed', top: '50%', alignSelf: 'flex-end'}} 
+            disableRipple>
+                <BsArrowRightCircleFill className={`pagination-arrow 
+                ${typeof totalGameCount === 'number' && (page * rowsPerPage >= totalGameCount) ? 'inactive' : ''}`}/>
+            </IconButton>
+            
         </>
     )
 }
