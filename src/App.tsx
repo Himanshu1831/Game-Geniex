94af0aecff9f8b78b78a3a95ce7d3697076a1a92
Serey Roth <sereyratanakroth@gmail.com>
2022-11-21 20:52:24 -0800

    set up routes and included side drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Box from '@mui/material/Box'
-import Games from './components/Games'
+
+import Games from './pages/Games'
+import BrowseDrawer from './components/BrowseDrawer'
+import SearchAppBar from './components/SearchAppBar'
+import Resources from './pages/Resources'
 
 function App() {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
     return (
         <Box sx={{
             width: '100vw',
             display: 'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'center',
-            position: 'relative',
         }}>
-            <Games />
+            <BrowseDrawer open={drawerOpen} setOpen={setDrawerOpen}/>
+            <Box sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                position: 'relative',
+                flex: 1,
+            }}>
+                <SearchAppBar toggleDrawer={setDrawerOpen} />
+                <Routes>
+                    <Route index element={<Games />} />
+                    <Route path='/games' element={<Games />} />
+                    <Route path='/:resource' element={<Resources />} />
+                </Routes>
+            </Box>
         </Box>
     )
 }
