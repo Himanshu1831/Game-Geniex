98a7268b204dc596ff2aed90564f81c93f23c512
Serey Roth <sereyratanakroth@gmail.com>
2022-11-21 21:00:21 -0800

    added new endpoints and generalized queries

--- src/redux/api/gameAPI.ts
+++ src/redux/api/gameAPI.ts
@@ -1,39 +1,48 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { GamesListType } from '../../features/typeGuards'
+import { TypeGuard } from '../../features'
+import { GamesListType, OtherTypeList } from '../../features/typeGuards'
 
 const API_KEY = '216afc1f9f044e1daafe76a78da14b71'
 
-type Query = {
+type PaginatedQuery = {
     page: number;
     pageCount: number;
+    endpoint: string;
 }
 
-type Args = Query | {}
+type Args = PaginatedQuery | { endpoint: string }
 
-const isQuery = (args: Args): args is Query => {
-    return (args as Query).page !== undefined && 
-    (args as Query).pageCount !== undefined
+const isPaginatedQuery = (args: Args): args is PaginatedQuery => {
+    return (args as PaginatedQuery).page !== undefined && 
+    (args as PaginatedQuery).pageCount !== undefined
 }
+
+const queryMaker = (args: Args) => {
+    if (isPaginatedQuery(args)) {
+        return `${args.endpoint}?page=${args.page}&page_size=${args.pageCount}&key=${API_KEY}`
+    }
+    return `${args.endpoint}?&key=${API_KEY}`
+}
+
+const transformFn = (typeGuard: TypeGuard<any>) => (rawResult: unknown) => typeGuard(rawResult);
 
 export const gameApi = createApi({
     reducerPath: 'gameApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.rawg.io/api/'}),
     endpoints:(builder) => ({
         getGamesList: builder.query<ReturnType<typeof GamesListType>, Args>({ 
-            query: (args) => {
-                if (isQuery(args)) {
-                    return `games?page=${args.page}&page_size=${args.pageCount}&key=${API_KEY}`
-                }
-                return `games?&key=${API_KEY}`
-            },
-            transformResponse: (rawResult: unknown) => {
-                return GamesListType(rawResult)
-            }
+            query: queryMaker,
+            transformResponse: transformFn(GamesListType),
+        }),
+        getResourceList: builder.query<ReturnType<typeof OtherTypeList>, Args>({
+            query: queryMaker,
+            transformResponse: transformFn(OtherTypeList),
         })
     })
 })
 
 export const {
-    useGetGamesListQuery
+    useGetGamesListQuery,
+    useGetResourceListQuery
 } = gameApi
