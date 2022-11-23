2b9096313b75c90be0a2e238a47e63957d287fd6
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 14:31:24 -0800

    created tabular pagination for table

---
+++ src/components/pagination/TabularPagination.tsx
@@ -0,0 +1,36 @@
+import React, { startTransition } from 'react'
+
+import { TablePagination } from '@mui/material'
+import { PaginationProps } from '.';
+
+interface Props extends PaginationProps {
+    rowsPerPage: number, 
+    setRowsPerPage: React.Dispatch<React.SetStateAction<number>>,
+}
+
+const Pagination = ({ page, rowsPerPage, setPage, setRowsPerPage }: Props) => {
+    const handleChangePage = (event: unknown, newPage: number) => {
+        startTransition(() => {
+            setPage(newPage);
+        })
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        startTransition(() => {
+            setRowsPerPage(parseInt(event.target.value, 10));
+        })
+    };
+
+    return (
+        <TablePagination
+        rowsPerPageOptions={[5, 10, 20]}
+        component='div'
+        count={-1}
+        rowsPerPage={rowsPerPage}
+        page={page}//0-based
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage} />
+    )
+}
+
+export default Pagination
