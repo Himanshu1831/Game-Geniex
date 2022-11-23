4ada78ab3a7c5fdee5578d950193929eaa4ed877
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 14:30:06 -0800

    added totalCount param

--- src/components/pagination/index.ts
+++ src/components/pagination/index.ts
@@ -1,4 +1,7 @@
 export interface PaginationProps {
     page: number, 
     setPage: React.Dispatch<React.SetStateAction<number>>,
+    totalCount?: number | undefined,
 }
+
+export const ItemsPerPage = 10;
