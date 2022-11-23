c0035cd94c586d341cff494fc84a753cd6dfb0a8
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 14:33:03 -0800

    added option for queries with search param

--- src/redux/api/gameAPI.ts
+++ src/redux/api/gameAPI.ts
@@ -5,23 +5,38 @@ import { GamesListType, OtherTypeList } from '../../features/typeGuards'
 
 const API_KEY = '216afc1f9f044e1daafe76a78da14b71'
 
-type PaginatedQuery = {
-    page: number;
-    pageCount: number;
+interface Query {
     endpoint: string;
 }
 
-type Args = PaginatedQuery | { endpoint: string }
+interface PaginatedQuery extends Query {
+    page: number;
+    pageCount: number;
+}
+
+interface SearchQuery extends PaginatedQuery {
+    search: string;
+}
+
+type Args = Query | PaginatedQuery | SearchQuery
 
 const isPaginatedQuery = (args: Args): args is PaginatedQuery => {
     return (args as PaginatedQuery).page !== undefined && 
     (args as PaginatedQuery).pageCount !== undefined
 }
 
+const isSearchQuery = (args: Args): args is SearchQuery => {
+    return (args as SearchQuery).search !== undefined 
+    && (args as SearchQuery).search !== ''
+}
+
 const queryMaker = (args: Args) => {
+    if (isSearchQuery(args)){
+        return `${args.endpoint}?page=${args.page}&page_size=${args.pageCount}&search=${args.search}&key=${API_KEY}`
+    }
     if (isPaginatedQuery(args)) {
         return `${args.endpoint}?page=${args.page}&page_size=${args.pageCount}&key=${API_KEY}`
-    }
+    } 
     return `${args.endpoint}?&key=${API_KEY}`
 }
 
