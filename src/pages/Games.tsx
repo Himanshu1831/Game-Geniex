63646196163c308cd137bed932c90ac1ecae0323
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 20:07:29 -0800

    added filters to queries and split games to modes

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -6,6 +6,7 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import Typography from '@mui/material/Typography'
 import Backdrop from '@mui/material/Backdrop'
 import CircularProgress from '@mui/material/CircularProgress'
+import Grid from '@mui/material/Grid'
 import { BsTable, BsCardText } from 'react-icons/bs'
 
 import GameTable from '../components/table/GameTable'
@@ -14,22 +15,66 @@ import Cards from '../components/cards/Cards'
 import Pagination from '../components/pagination/Pagination'
 
 import { useGetGamesListQuery } from '../redux/api/gameAPI'
-import { GamesListType } from '../features/typeGuards'
 import PaginationArrows from '../components/pagination/PaginationArrows'
 import { ItemsPerPage } from '../components/pagination'
 import TabularPagination from '../components/pagination/TabularPagination'
 import { useAppSelector } from '../features/hooks'
-import { Divider } from '@mui/material'
+import { TypeGuard } from '../features'
+import FilterMenu from '../components/filters/FilterMenu'
+import { Filter, FiltersState } from '../redux/features/filtersSlice'
 
-export interface GamesProps {
-    readonly data: ReturnType<typeof GamesListType> | undefined;
+interface GamesProps {
+    readonly results: Array<ReturnType<TypeGuard<any>>> | undefined;
     readonly isFetching: boolean;
+}
+
+interface CardsProps extends GamesProps {
+    readonly page: number;
+    readonly setPage: React.Dispatch<React.SetStateAction<number>>;
+    readonly totalCount: number | undefined;
+    readonly element: (props: any) => JSX.Element;
+}
+
+interface TableProps extends GamesProps {
+    readonly page: number;
+    readonly setPage: React.Dispatch<React.SetStateAction<number>>;
     readonly rowsPerPage: number;
+    readonly setRowsPerPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 enum Mode {
     Table,
     Cards
+}
+
+const CardMode = ({ results, page, setPage, isFetching, totalCount, element }: CardsProps) => (
+    <>
+        <Cards results={results} isFetching={isFetching} element={element} />
+        <Pagination page={page + 1} setPage={setPage} totalCount={totalCount} />
+    </>
+)
+
+const TableMode = ({results, isFetching, page, rowsPerPage, setPage, setRowsPerPage}: TableProps) => (
+    <>
+        <GameTable results={results} isFetching={isFetching} rowsPerPage={rowsPerPage} />
+        <TabularPagination page={page} rowsPerPage={rowsPerPage} setPage={setPage}
+            setRowsPerPage={setRowsPerPage} />
+    </>
+)
+
+const makeQuery = (page: number, pageCount: number, endpoint: string, search: string, filters: FiltersState) => {
+    const query = {
+        page,
+        pageCount,
+        endpoint,
+        search,
+    }
+    Object.keys(filters).map((key: string) => {
+        if ((filters[key] as string[]).length > 0) {
+            query[key] = (filters[key] as Filter[]).map(filter => filter.id).join(',')
+        }
+    })
+    return query;
 }
 
 const Games = () => {
@@ -39,13 +84,15 @@ const Games = () => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10)
     const search = useAppSelector(state => state.app.search)
+    const filters = useAppSelector(state => state.filters);
 
-	const { data, isFetching, error } = useGetGamesListQuery({
-        page: page + 1,
-        pageCount: mode === Mode.Table ? rowsPerPage : ItemsPerPage, 
-        endpoint: 'games',
-        search,
-    });
+	const { data, isFetching, error } = useGetGamesListQuery(makeQuery(
+        page + 1, 
+        mode === Mode.Table ? rowsPerPage : ItemsPerPage, 
+        'games', 
+        search, 
+        filters
+    ));
 
     const handleChangeMode = (event: React.MouseEvent<HTMLElement>, newMode: Mode) => {
         startTransition(() => {
@@ -74,19 +121,33 @@ const Games = () => {
                     <ToggleButton value={Mode.Cards}><BsCardText /></ToggleButton>
                 </ToggleButtonGroup>
             </Box>
+            <Grid container width='100%' spacing={1} padding={1}>
+                {Object.keys(filters).map(filter => (
+                    <Grid item xs={4} md={2} key={filter}>
+                        <FilterMenu key={filter} filterType={filter} />
+                    </Grid>
+                ))}
+            </Grid>
             <Typography variant='body2' alignSelf='center' marginBottom={1.5}>
                 {isFetching ? 'Loading...' : `${data?.count || 0} games found`}
             </Typography>
             {mode === Mode.Table ? (
-                <>
-                    <GameTable data={data} isFetching={isFetching} rowsPerPage={rowsPerPage} />
-                    <TabularPagination page={page} rowsPerPage={rowsPerPage} setPage={setPage}
-                    setRowsPerPage={setRowsPerPage} />
-                </>
+                <TableMode 
+                results={data?.results} 
+                isFetching={isFetching} 
+                rowsPerPage={rowsPerPage} 
+                page={page} 
+                setPage={setPage}
+                setRowsPerPage={setRowsPerPage} />
             ) : (
                 <>
-                    <Cards results={data?.results} isFetching={isFetching} element={GameCard} />
-                    <Pagination page={page + 1} setPage={setPage} totalCount={data?.count} />
+                    <CardMode 
+                    results={data?.results} 
+                    isFetching={isFetching} 
+                    element={GameCard} 
+                    page={page} 
+                    setPage={setPage} 
+                    totalCount={data?.count} />
                 </>
             )}
             <PaginationArrows page={page} setPage={setPage} rowsPerPage={rowsPerPage} totalCount={data?.count} />
