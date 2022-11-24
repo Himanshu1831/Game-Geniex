31a75dd9dc8428377dc84891d2875dcdf35f1fff
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 20:05:11 -0800

    fixed import path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-import { store } from './redux/api/store'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
