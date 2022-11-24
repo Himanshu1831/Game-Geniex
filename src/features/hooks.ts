3b5f8437f407805f585e8cedfa11cc116a941ee9
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 20:06:40 -0800

    fixed import paths

--- src/features/hooks.ts
+++ src/features/hooks.ts
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, Dispatch } from "../redux/api/store";
+import type { RootState, Dispatch } from "../redux/store";
 
 export const useAppDispatch: () => Dispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
