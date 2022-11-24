3886daae1f13d53dc83202d1e33b9d8925a13420
Serey Roth <sereyratanakroth@gmail.com>
2022-11-23 20:08:15 -0800

    moved interfaces and query makers

--- src/redux/api/gameAPI.ts
+++ src/redux/api/gameAPI.ts
@@ -1,46 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Args, queryMaker, transformFn } from '.'
 
-import { TypeGuard } from '../../features'
 import { GamesListType, OtherTypeList } from '../../features/typeGuards'
-
-const API_KEY = '216afc1f9f044e1daafe76a78da14b71'
-
-interface Query {
-    endpoint: string;
-}
-
-interface PaginatedQuery extends Query {
-    page: number;
-    pageCount: number;
-}
-
-interface SearchQuery extends PaginatedQuery {
-    search: string;
-}
-
-type Args = Query | PaginatedQuery | SearchQuery
-
-const isPaginatedQuery = (args: Args): args is PaginatedQuery => {
-    return (args as PaginatedQuery).page !== undefined && 
-    (args as PaginatedQuery).pageCount !== undefined
-}
-
-const isSearchQuery = (args: Args): args is SearchQuery => {
-    return (args as SearchQuery).search !== undefined 
-    && (args as SearchQuery).search !== ''
-}
-
-const queryMaker = (args: Args) => {
-    if (isSearchQuery(args)){
-        return `${args.endpoint}?page=${args.page}&page_size=${args.pageCount}&search=${args.search}&key=${API_KEY}`
-    }
-    if (isPaginatedQuery(args)) {
-        return `${args.endpoint}?page=${args.page}&page_size=${args.pageCount}&key=${API_KEY}`
-    } 
-    return `${args.endpoint}?&key=${API_KEY}`
-}
-
-const transformFn = (typeGuard: TypeGuard<any>) => (rawResult: unknown) => typeGuard(rawResult);
 
 export const gameApi = createApi({
     reducerPath: 'gameApi',
