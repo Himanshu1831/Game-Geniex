e4c4a3347d256e580203e4f98d6df50c05c4e36e
Serey Roth <sereyratanakroth@gmail.com>
2022-11-24 18:22:55 -0800

    renamed FilterMenu as dropdown

---
+++ src/components/filters/FilterDropdown.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useCallback, useRef } from 'react';
+import { GoChevronUp, GoChevronDown } from 'react-icons/go'
+
+import Button from '@mui/material/Button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow'
+import Popper from '@mui/material/Popper';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+
+import FilterMenuContent from './FilterMenuContent';
+import { MenuList } from '@mui/material';
+
+interface Props {
+    filterType: string;
+}
+
+const FilterDropdown = ({ filterType }: Props) => {
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef<HTMLButtonElement>(null);
+
+    const handleToggle = useCallback(() => {
+        setOpen(prevOpen => !prevOpen)
+    }, [])
+
+    const handleClose = useCallback((event: Event | React.SyntheticEvent) => {
+        if (
+            anchorRef.current &&
+            anchorRef.current.contains(event.target as HTMLElement)
+        ) {
+            return;
+        }
+        setOpen(false);
+    }, [anchorRef])
+
+    return (
+        <div>
+            <Button
+                ref={anchorRef}
+                id="composition-button"
+                aria-controls={open ? 'composition-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+                endIcon={open ? <GoChevronUp /> : <GoChevronDown />}
+                variant='contained'
+                sx={{
+                    textTransform: 'capitalize',
+                    width: '100%',
+                }}
+            >
+                <Typography
+                    flex={1}
+                    textAlign='left'
+                    textTransform='capitalize'>
+                    {filterType}
+                </Typography>
+            </Button>
+            <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                placement="bottom-start"
+                transition
+                disablePortal
+                sx={{ zIndex: 20, padding: 0 }}
+            >
+                {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin:
+                                placement === 'bottom-start' ? 'left top' : 'left bottom',
+                        }}
+                    >
+                        <Paper sx={{ width: 300, padding: 1, bgcolor: 'azure' }}>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList
+                                autoFocusItem={open}
+                                id='composition-menu'
+                                aria-aria-labelledby='composition-button'
+                                sx={{ padding: 1 }}>
+                                    <FilterMenuContent filterType={filterType} />
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+        </div>
+    );
+}
+
+export default FilterDropdown;
