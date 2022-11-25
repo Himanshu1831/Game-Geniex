7a8985422f03001e61f580d971d83f845a8b5912
Serey Roth <sereyratanakroth@gmail.com>
2022-11-24 18:22:09 -0800

    created drawer for filters in mobile mode

---
+++ src/components/filters/FilterDrawer.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+
+import { GoChevronRight } from 'react-icons/go'
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import Typography from '@mui/material/Typography';
+import MenuList from '@mui/material/MenuList'
+import { styled } from '@mui/material/styles';
+
+import { useAppSelector } from '../../features/hooks';
+import FilterMenuContent from './FilterMenuContent';
+
+const drawerWidth = 250;
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+
+interface Props {
+    open: boolean;
+    handleDrawerToggle: () => void;
+}
+
+export default function FilterDrawer({ open, handleDrawerToggle }: Props ) {
+    const filters = useAppSelector(state => state.filters);
+
+    const [menu, setMenu] = useState('main');
+    const isFilterType = Object.keys(filters).includes(menu);
+
+    const drawer = (
+        <Box component={Paper} sx={{ flex: 1, width: drawerWidth }}>
+            <DrawerHeader>
+                <Typography sx={{ flex: 1, paddingX: 1, textTransform: 'capitalize' }}>
+                    filter by{isFilterType && `: ${menu}`}
+                </Typography>
+                <IconButton 
+                onClick={isFilterType ? () => setMenu('main') : handleDrawerToggle} 
+                >
+                    <GoChevronRight />
+                </IconButton>
+            </DrawerHeader>
+            <Divider />
+            {menu === 'main' ? (
+                Object.keys(filters).map((filter: string) => (
+                    <ListItem key={filter} disablePadding>
+                        <ListItemButton onClick={() => setMenu(filter)}>
+                            <ListItemText primary={filter} sx={{textTransform: 'capitalize'}} />
+                        </ListItemButton>
+                    </ListItem>
+                ))
+            ) : (
+                <MenuList
+                autoFocusItem={open}
+                id='composition-menu'
+                aria-aria-labelledby='composition-button'
+                sx={{ padding: 1 }}>
+                    <FilterMenuContent filterType={menu} />
+                </MenuList>
+            )}
+        </Box>
+    );
+
+    return (
+        <Box
+            sx={{ width: drawerWidth, display: 'flex', flexDirection: 'column', height: '100%' }}
+            aria-label="filter drawer"
+        >
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Drawer
+                variant="temporary"
+                anchor='right'
+                open={open}
+                onClose={handleDrawerToggle}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on open.
+                }}
+            >
+                {drawer}
+            </Drawer>
+        </Box>
+    );
+}
