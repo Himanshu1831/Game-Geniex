47717c4bdbb85e70d013ed884d9453d1ac54d696
Serey Roth <sereyratanakroth@gmail.com>
2022-11-24 18:24:00 -0800

    extracted SelectableMenuItem component

---
+++ src/components/filters/SelectableMenuItem.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+import MenuItem from '@mui/material/MenuItem';
+import Checkbox from '@mui/material/Checkbox'
+import Typography from '@mui/material/Typography';
+
+import { useAppDispatch, useAppSelector } from '../../features/hooks';
+import { addFilter, Filter, removeFilter } from '../../redux/features/filtersSlice'
+
+const SelectableMenuItem = ({ name, id, filterType }
+    : { name: string, id: number, filterType: string }) => {
+    const dispatch = useAppDispatch();
+    const filters = useAppSelector(state => state.filters)
+    const [selected, setSelected] = useState((filters[filterType] as Filter[]).map((filter: Filter) => filter.id).includes(id));
+
+    const handleClick = () => {
+        if (!selected) dispatch(addFilter({ name, id, filterType }))
+        else dispatch(removeFilter({ name, id, filterType }))
+
+        setSelected(prev => !prev)
+    }
+
+    return (
+        <>
+            <MenuItem onClick={handleClick} sx={{
+                display: 'flex',
+            }} disableTouchRipple>
+                <Checkbox
+                    sx={{ padding: 0, marginRight: 1 }}
+                    checked={selected} />
+                <Typography
+                    fontSize={12}
+                    sx={{ overflow: 'hidden', textOverflow: 'ellipsis', textTransform: 'capitalize' }}>
+                    {name}
+                </Typography>
+            </MenuItem>
+        </>
+    )
+}
+
+export default SelectableMenuItem
