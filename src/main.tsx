6f0dcafe1dc71788c4b3c501034256bfb24a136c
Serey Roth <sereyratanakroth@gmail.com>
2022-11-25 23:33:36 -0800

    removed browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
@@ -10,8 +9,6 @@ import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <App />
     </Provider>
 )
