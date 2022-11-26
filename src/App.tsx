a709cbfb2d2bdaa159aefa845c07a3ae7a991441
Serey Roth <sereyratanakroth@gmail.com>
2022-11-25 17:45:29 -0800

    removed drawer and search app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
 import Box from '@mui/material/Box'
 
 import Games from './pages/Games'
-import BrowseDrawer from './components/BrowseDrawer'
-import SearchAppBar from './components/SearchAppBar'
-import Resources from './pages/Resources'
-
 function App() {
-    const [drawerOpen, setDrawerOpen] = useState(false);
 
     return (
         <Box sx={{
@@ -18,23 +11,7 @@ function App() {
             alignItems: 'center',
             justifyContent: 'center',
         }}>
-            <BrowseDrawer open={drawerOpen} setOpen={setDrawerOpen}/>
-            <Box sx={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                position: 'relative',
-                flex: 1,
-            }}>
-                <SearchAppBar toggleDrawer={setDrawerOpen} />
-                <Routes>
-                    <Route index element={<Games />} />
-                    <Route path='/games' element={<Games />} />
-                    <Route path='/:resource' element={<Resources />} />
-                </Routes>
-            </Box>
+            <Games />
         </Box>
     )
 }
