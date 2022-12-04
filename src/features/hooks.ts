258fffcfa8efddf59f8e3d3d8c3a3805d837bb12
Serey Roth <sereyratanakroth@gmail.com>
2022-12-03 17:42:30 -0800

    no longer using redux toolkit

--- src/features/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector, useDispatch } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, Dispatch } from "../redux/store";
-
-export const useAppDispatch: () => Dispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
