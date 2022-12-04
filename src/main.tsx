140287c794e437e96a152629600ab68fe819203f
Serey Roth <sereyratanakroth@gmail.com>
2022-12-03 17:37:18 -0800

    added query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import App from './App'
 import './index.css'
 
-import { store } from './redux/store'
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
         <App />
-    </Provider>
+    </QueryClientProvider>
 )
