6b73f8a0dc40e903ca92198b4ae6aa3a0942a579
Serey Roth <sereyratanakroth@gmail.com>
2022-12-03 17:40:26 -0800

    fixed props and handlers

--- src/components/filters/SelectableMenuItem.tsx
+++ src/components/filters/SelectableMenuItem.tsx
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import MenuItem from '@mui/material/MenuItem';
 import Checkbox from '@mui/material/Checkbox'
 import Typography from '@mui/material/Typography';
 
-import { useAppDispatch, useAppSelector } from '../../features/hooks';
-import { addFilter, Filter, removeFilter } from '../../redux/features/filtersSlice'
-
 interface Props {
     name: string; 
     id: number; 
-    filterType: string;
+    onSelect: (name: string, id: number, isSelectedBefore: boolean) => void;
+    isSelected: boolean;
 }
 
-const SelectableMenuItem = ({ name, id, filterType }: Props) => {
-    const dispatch = useAppDispatch();
-    const filters = useAppSelector(state => state.filters)
-    const [selected, setSelected] = useState((filters[filterType] as Filter[]).map((filter: Filter) => filter.id).includes(id));
-
-    const handleClick = () => {
-        if (!selected) dispatch(addFilter({ name, id, filterType }))
-        else dispatch(removeFilter({ name, id, filterType }))
-
-        setSelected(prev => !prev)
+const SelectableMenuItem = ({ name, id, isSelected, onSelect }: Props) => {
+    const handleSelect = () => {
+        onSelect(name, id, isSelected);
     }
 
     return (
         <>
-            <MenuItem onClick={handleClick} sx={{
+            <MenuItem 
+            onClick={handleSelect} 
+            sx={{
                 display: 'flex',
-            }} disableTouchRipple>
+            }} 
+            disableTouchRipple
+            >
                 <Checkbox
-                    sx={{ padding: 0, marginRight: 1 }}
-                    checked={selected} />
+                    sx={{ 
+                        padding: 0, 
+                        marginRight: 1 
+                    }}
+                    checked={isSelected} />
                 <Typography
                     fontSize={12}
-                    sx={{ overflow: 'hidden', textOverflow: 'ellipsis', textTransform: 'capitalize' }}>
+                    sx={{ 
+                        overflow: 'hidden', 
+                        textOverflow: 'ellipsis', 
+                        textTransform: 'capitalize' 
+                    }}>
                     {name}
                 </Typography>
             </MenuItem>
