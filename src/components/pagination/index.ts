fd244e962b0eed9329cc33e0ffa6909204ade39b
Serey Roth <sereyratanakroth@gmail.com>
2022-12-03 17:41:02 -0800

    removed pagination props

--- src/components/pagination/index.ts
+++ src/components/pagination/index.ts
@@ -1,7 +1,1 @@
-export interface PaginationProps {
-    page: number, 
-    setPage: React.Dispatch<React.SetStateAction<number>>,
-    totalCount?: number | undefined,
-}
-
 export const ItemsPerPage = 10;
