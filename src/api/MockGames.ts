19aae9540dc23cc84d58fb266e8faabd073c645f
Serey Roth <sereyratanakroth@gmail.com>
2022-12-03 22:18:41 -0800

    created mock data

---
+++ src/api/MockGames.ts
@@ -0,0 +1,116 @@
+export const MOCK_GAMES = {
+    count: 5,
+    results: [
+        {
+            id: 3498, 
+            slug: 'grand-theft-auto-v',
+            name: 'Grand Theft Auto V',
+            released: '2013-09-17',
+            background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+            genres: [
+                { slug: 'action', name: 'Action' },
+                { slug: 'adventure', name: 'Adventure' }
+            ],
+            metacritic: 91,
+            rating: 4.47,
+            tags: [
+                { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+                { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+                { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+            ]
+        }, 
+        {
+            id: 3499, 
+            slug: 'grand-theft-auto-vi',
+            name: 'Grand Theft Auto VI',
+            released: '2014-05-10',
+            background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+            genres: [
+                { slug: 'action', name: 'Action' },
+                { slug: 'adventure', name: 'Adventure' },
+                { slug: 'shooter', name: 'Shooter' }
+            ],
+            metacritic: 92,
+            rating: 4.44,
+            tags: [
+                { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+                { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+                { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+            ]
+        },
+        {
+            id: 3500, 
+            slug: 'grand-theft-auto-vii',
+            name: 'Grand Theft Auto VII',
+            released: '2016-09-17',
+            background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+            genres: [
+                { slug: 'action', name: 'Action' },
+                { slug: 'adventure', name: 'Adventure' },
+                { slug: 'shooter', name: 'Shooter' },
+            ],
+            metacritic: 91,
+            rating: 4.47,
+            tags: [
+                { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+                { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+                { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+            ]
+        },
+        {
+            id: 3501, 
+            slug: 'grand-theft-auto-viii',
+            name: 'Grand Theft Auto VIII',
+            released: '2013-09-17',
+            background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+            genres: [
+                { slug: 'action', name: 'Action' },
+                { slug: 'adventure', name: 'Adventure' }
+            ],
+            metacritic: 91,
+            rating: 4.47,
+            tags: [
+                { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+                { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+                { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+            ]
+        },
+        {
+            id: 3502, 
+            slug: 'grand-theft-auto-ix',
+            name: 'Grand Theft Auto IX',
+            released: '2013-09-17',
+            background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+            genres: [
+                { slug: 'action', name: 'Action' },
+                { slug: 'adventure', name: 'Adventure' }
+            ],
+            metacritic: 91,
+            rating: 4.47,
+            tags: [
+                { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+                { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+                { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+            ]
+        }
+    ]
+}
+
+export const MOCK_GAME = {
+    id: 3498, 
+    slug: 'grand-theft-auto-v',
+    name: 'Grand Theft Auto V',
+    released: '2013-09-17',
+    background_image: 'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg',
+    genres: [
+        { slug: 'action', name: 'Action' },
+        { slug: 'adventure', name: 'Adventure' }
+    ],
+    metacritic: 91,
+    rating: 4.47,
+    tags: [
+        { id: 31, slug: 'singleplayer', name: 'Singleplayer' },
+        { id: 7, slug: 'multiplayer', name: 'Multiplayer' },
+        { id: 13, slug: 'atmospheric', name: 'atmospheric' }
+    ]
+}
